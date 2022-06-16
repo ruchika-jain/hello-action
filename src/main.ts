2dc2e53e0ed3d5af4cf2cf481a4ddc91ec4b30db
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-06-16 15:55:45 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    console.log("Before waiting!");
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
+    console.log("Done waiting!!");
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
